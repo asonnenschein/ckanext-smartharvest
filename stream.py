bdf1d737e01fe6990dae6f2dcf44b3374e20e78a
asonnenschein <adrian.sonnenschein@gmail.com>
2014-06-01 13:52:08 -0700

    understanding sax streams

--- stream.py
+++ stream.py
@@ -1,17 +1,43 @@
-csw = "http://geothermaldata.org/csw?request=GetRecords&service=CSW&version=2.0.2&resultType=results&outputSchema=http://www.isotc211.org/2005/gmd&typeNames=csw:Record&elementSetName=summary&maxRecords=10"
-
 import requests
 import xml.sax
 import xml.sax.handler
 
-class SAXHandler(xml.sax.handler.ContentHandler):
+url = "http://geothermaldata.org/csw"
+payload = {"request": "GetRecords",
+		   "service": "CSW",
+		   "version": "2.0.2",
+		   "resultType": "results",
+		   "outputSchema": "http://www.isotc211.org/2005/gmd",
+		   "typeNames": "csw:Record",
+		   "elementSetName": "summary",
+		   "maxRecords": "1"}
+
+class SAXHandler(xml.sax.ContentHandler):
+	def __init__(self):
+		self.parent_flag = False
+		self.record = {}
+		self.tag = ""
+
 	def startElement(self, name, attrs):
-		print name, attrs
+		if name == "gmd:MD_Metadata":
+			self.parent_flag = True
+		if self.parent_flag:
+			self.record[name] = ""
+			self.tag = name
 
-r = requests.get(csw, stream=True)
+#			print name, dict(attrs)
+#			self.child_list.append(name)
+	
+	def characters(self, content):
+		print self.tag, content
+		self.record[self.tag] += content
 
-#d = xml.sax.parse(data, SAXHandler())
+	def endElement(self, name):
+		if name == "gmd:MD_Metadata":
+			self.parent_flag = False
+#			print self.record
 
-parser = xml.sax.make_parser()
-parser.setContentHandler(SAXHandler())
-parser.parse(r.raw)
+response = requests.get(url, params=payload, stream=True)
+raw_response = response.raw
+
+xml.sax.parse(raw_response, SAXHandler())
